21e3af906c10e78415d500624a58d00d64859506
abaltaza <andrebaltazarpinto@gmail.com>
2024-08-19 18:03:29 +0100

    localStorage

---
+++ Front End Carrer Path Scrimba/02. Making websites interactive/10. JavaScript challenges - part 3/save to localStorage/index.js
@@ -0,0 +1,10 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+//1
+localStorage.setItem("programming", "difficult")
+
+let programming = localStorage.getItem("programming")
+
+console.log(programming)
