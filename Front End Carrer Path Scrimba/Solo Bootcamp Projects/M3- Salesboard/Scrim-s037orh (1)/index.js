6dbb169c71c946b6d0f8db3ac4b79e4fee87540d
abaltaza <andrebaltazarpinto@gmail.com>
2024-08-21 14:07:14 +0100

    resolve bugs

--- Front End Carrer Path Scrimba/Solo Bootcamp Projects/M3- Salesboard/Scrim-s037orh (1)/index.js
+++ Front End Carrer Path Scrimba/Solo Bootcamp Projects/M3- Salesboard/Scrim-s037orh (1)/index.js
@@ -11,3 +11,104 @@ let productB = {
     revenue: 300,
     commision: 75
 }
+
+let achievements = {
+    bell: "🔔",
+    money: "💰",
+    trophy: "🏆"
+}
+
+//variables
+//fire and star
+const star = document.getElementById("star");
+const fire = document.getElementById("fire");
+
+let salesTitle = document.querySelector(".sales-heading")
+let achievementsTitle = document.querySelector(".achievements-heading")
+
+console.log(achievementsTitle)
+//board for live sales
+let liveSalesBoard = document.getElementById("live-sales")
+let listAchievements = document.querySelector(".live-achievements")
+// Total Revenue board
+let totalRevenueBoard = document.getElementById("total-revenue")
+// Total comission board
+let totalComissionBoard = document.getElementById("commission-value")
+
+// arrays
+
+// Loops
+
+// functions
+const maxElements = 20;
+let countElement = 0
+let liveAchievements = 0;
+let totalRevenue = 0;
+let totalComission = 0;
+
+function addStar() {
+    if(countElement < maxElements) {
+        console.log("clicked")
+        liveSalesBoard.innerText += productA.emoji ;
+        totalRevenue += productA.revenue;
+        totalComission += productA.commision;
+        updateBoards();
+        countElement++;
+        checkAchievements();
+    }else {
+        alert("Maximum number of elements (star & fire)")
+    }
+
+
+    if(countElement === 1) {
+        listAchievements.innerText += achievements.bell
+        liveAchievements++;
+    }
+
+}
+
+function addFire() {
+    if (countElement < maxElements) {
+        liveSalesBoard.innerText += productB.emoji;
+        totalRevenue += productB.revenue;
+        totalComission += productB.commision;
+        updateBoards();
+        countElement++;
+        checkAchievements();
+    }else {
+        alert("Maximum number of elements (star & fire)")
+    }
+    salesTitle.innerText = `Live Sales - ${countElement}`
+
+}
+
+function updateBoards() {
+    totalRevenueBoard.innerText = totalRevenue;
+    totalComissionBoard.innerText = totalComission;
+    salesTitle.innerText = `Live Sales - ${countElement}`;
+    achievementsTitle.innerText = `Live Achievements - ${liveAchievements}`
+}
+
+function checkAchievements() {
+    if (countElement === 1)  {
+        listAchievements.innerText += achievements.bell;
+        liveAchievements++;
+    }else if (totalRevenue >= 2500){
+        listAchievements.innerText += achievements.money
+        liveAchievements++;
+    }else if (countElement === 15) {
+        listAchievements.innerText += achievements.trophy
+        liveAchievements++;
+    }
+}
+
+
+
+// event listeners
+star.addEventListener("click", addStar)
+fire.addEventListener("click", addFire)
+
+
+
+
+
