802fbc09ea4d901b7c3abe2ba029694bac3e61c9
abaltaza <andrebaltazarpinto@gmail.com>
2024-08-22 22:40:30 +0100

    working on Firebase

--- Front End Carrer Path Scrimba/Solo Bootcamp Projects/M3 - Choreslist (to-do app)/choreslist -scrim/index.js
+++ Front End Carrer Path Scrimba/Solo Bootcamp Projects/M3 - Choreslist (to-do app)/choreslist -scrim/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js"
-import { getDatabase , ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js"
+import { getDatabase , ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js"
 
 // 
 
@@ -7,16 +7,15 @@ const firebaseConfig = {
     databaseURL: "https://chorelist-e6d57-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-
-
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
 const referenceinDB = ref(database, "chorelist")
 
-
 let inputEl = document.querySelector("#input-el")
-const addChoreBtn = document.querySelector(".add-chore-list-btn")
 const ulEl = document.getElementById("ul-el")
+const addChoreBtn = document.querySelector(".add-chore-list-btn")
+const deleteAllButtonEl = document.getElementById("delete-btn")
+
 
 function render(myTodos) {
     let listTodos = ""
@@ -32,8 +31,6 @@ function render(myTodos) {
 
     for (let i = 0; i < myTodos.length; i++) {
         const checkbox = document.getElementById(`toDo-${i}`);
-        console.log(`Adding listener to #toDo-${i}`, checkbox);
-
         if (checkbox){
             checkbox.addEventListener("change", () => hideItemDelayed(i))
         }else {
@@ -44,12 +41,18 @@ function render(myTodos) {
 }
 
 onValue(referenceinDB, function (snapshot) {
-    const snapshotValues = snapshot.val()
-    const myTodos = Object.values(snapshotValues)
-    render(myTodos)
-    console.log(myTodos)
+    
+    if(snapshot) {
+        const snapshotValues = snapshot.val()
+        const myTodos = Object.values(snapshotValues)
+        render(myTodos)
+    }
 })
 
+deleteAllButtonEl.addEventListener("dblclick", function() {
+    remove(referenceinDB)
+    ulEl.innerHTML = "";
+})
 
 addChoreBtn.addEventListener("click", function() {
     push(referenceinDB, inputEl.value)
@@ -65,8 +68,9 @@ function hideItemDelayed(index) {
     if(checkbox && checkbox.checked) {
         setTimeout(() => {
             console.log(`Trying to remove item-${index}`);
-            console.log("item", item)
+
              if(item) {
+                removeItemFromFirebase(index)
                 item.remove();
              } else {
                 console.error(`Element with id item-${index} not found after delay`)
@@ -75,40 +79,25 @@ function hideItemDelayed(index) {
     }
 }
 
+function removeItemFromFirebase(index) {
+    const chorelistRef = ref(database, 'chorelist');
 
+    onValue(chorelistRef, (snapshot) => {
+        const data = snapshot.val();
 
-// const removeAllBtn= document.querySelector(".delete-btn")
+        Object.entries(data).forEach(([key, value], i) => {
+            if (i === index) {
+                const itemRef = ref(database, `chorelist/${key}`)
 
-
-
-
-// //const app = initializeApp(appSettings);
-// const database = getDatabase(app)
-// const endorsementsInDB = ref(database, "endorsements")
-
-// function addToDo() {
-//     console.log("clicked")
-// }
-// console.log(myTodos)
-
-// function removeAll() {
-//     console.log("remove")
-// }
-// addChoreBtn.addEventListener("click",addToDo)
-// removeAllBtn.addEventListener("click", removeAll)
-
-// const inputText = document.querySelector("#list-app")
-// inputText.value = `hello`
-
-// console.log(inputText)
-
-// /* creating the new li for the to do app */
-
-
-
-// let newEl = document.createElement('li');
-//     //create the content for doing
-//     newEl.innerHTML = `
-    
-//     `;
+                remove(itemRef)
+                .then(() => {
+                    console.log(`Item with index ${index} has been removed from Firebase`)
+                })
+                .catch(error => {
+                    console.error(`Failed to remove item from Firebase: ${error}`)
+                });
+            }
+        })
+    }); 
+}
 
