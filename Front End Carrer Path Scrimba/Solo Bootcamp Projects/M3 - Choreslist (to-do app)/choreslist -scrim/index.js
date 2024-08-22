54f911c9bbea2f55fb370f123112f4605728ca85
abaltaza <andrebaltazarpinto@gmail.com>
2024-08-22 18:24:20 +0100

    database working

--- Front End Carrer Path Scrimba/Solo Bootcamp Projects/M3 - Choreslist (to-do app)/choreslist -scrim/index.js
+++ Front End Carrer Path Scrimba/Solo Bootcamp Projects/M3 - Choreslist (to-do app)/choreslist -scrim/index.js
@@ -1,42 +1,59 @@
-import { initializeApp } from "https://chorelist-e6d57-default-rtdb.europe-west1.firebasedatabase.app/"
-import { getDatabase, ref, push, onValue, remove } from "https://chorelist-e6d57-default-rtdb.europe-west1.firebasedatabase.app/"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js"
+import { getDatabase , ref, push } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js"
+
+// 
+
+const firebaseConfig = {
+    databaseURL: "https://chorelist-e6d57-default-rtdb.europe-west1.firebasedatabase.app/"
+}
 
 
-let myTodos = [];
-const inputEl = document.querySelector("#input-el")
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceinDB = ref(database, "chorelist")
+
+
+let inputEl = document.querySelector("#input-el")
 const addChoreBtn = document.querySelector(".add-chore-list-btn")
-const removeAllBtn= document.querySelector(".delete-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("To-do"))
 
+addChoreBtn.addEventListener("click", function() {
+    push(referenceinDB, inputEl.value)
+    inputEl = ""
+})
 
-if (leadsFromLocalStorage) {
-    myTodos = leadsFromLocalStorage
-    /*render(myTodos)*/
-}
-
-function addToDo() {
-    console.log("clicked")
-}
-console.log(myTodos)
-
-function removeAll() {
-    console.log("remove")
-}
-addChoreBtn.addEventListener("click",addToDo)
-removeAllBtn.addEventListener("click", removeAll)
-
-const inputText = document.querySelector("#list-app")
-inputText.value = `hello`
-
-console.log(inputText)
-
-/* creating the new li for the to do app */
+// const removeAllBtn= document.querySelector(".delete-btn")
 
 
 
-let newEl = document.createElement('li');
-    //create the content for doing
-    newEl.innerHTML = `
+
+// //const app = initializeApp(appSettings);
+// const database = getDatabase(app)
+// const endorsementsInDB = ref(database, "endorsements")
+
+// function addToDo() {
+//     console.log("clicked")
+// }
+// console.log(myTodos)
+
+// function removeAll() {
+//     console.log("remove")
+// }
+// addChoreBtn.addEventListener("click",addToDo)
+// removeAllBtn.addEventListener("click", removeAll)
+
+// const inputText = document.querySelector("#list-app")
+// inputText.value = `hello`
+
+// console.log(inputText)
+
+// /* creating the new li for the to do app */
+
+
+
+// let newEl = document.createElement('li');
+//     //create the content for doing
+//     newEl.innerHTML = `
     
-    `;
+//     `;
 
