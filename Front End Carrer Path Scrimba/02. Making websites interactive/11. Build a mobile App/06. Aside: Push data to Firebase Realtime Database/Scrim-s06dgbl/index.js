3ee4b824d9df10b2cf8a2c9c6acb740788766fe8
abaltaza <andrebaltazarpinto@gmail.com>
2024-08-24 08:34:18 +0100

    setting up the gift birdthays

---
+++ Front End Carrer Path Scrimba/02. Making websites interactive/11. Build a mobile App/06. Aside: Push data to Firebase Realtime Database/Scrim-s06dgbl/index.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
+
+const firebaseConfig = {
+     databaseURL: "https://birthdays-c0996-default-rtdb.europe-west1.firebasedatabase.app/"
+} 
+
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "birthdays")
+
+const birthdayInputField = document.getElementById("birthday-input")
+const submitButton = document.getElementById("submit-button")
+
+submitButton.addEventListener("click", function() {
+    push(referenceInDB, birthdayInputField.value)
+    birthdayInputField.value = ""
+})
