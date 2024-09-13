9cd06c39924d291b7f48163333045847e874314c
abaltaza <andrebaltazarpinto@gmail.com>
2024-09-13 10:54:00 +0100

    second photo and prettier all

--- Front End Carrer Path Scrimba/04. Essential CSS concepts/02. CSS Fundamentals: Challenges/Solo Project: Oldagram/Scrim-s01kgac/index.js
+++ Front End Carrer Path Scrimba/04. Essential CSS concepts/02. CSS Fundamentals: Challenges/Solo Project: Oldagram/Scrim-s01kgac/index.js
@@ -1,8 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getDatabase, ref, set, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js"
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js'
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+  onValue,
+  remove,
+} from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js'
 
 const appSettings = {
-  databaseURL: "https://oldagram-2079c-default-rtdb.europe-west1.firebasedatabase.app/"
+  databaseURL:
+    'https://oldagram-2079c-default-rtdb.europe-west1.firebasedatabase.app/',
 }
 
 //initialize firebase
@@ -10,9 +18,7 @@ const app = initializeApp(appSettings)
 const db = getDatabase(app)
 
 // Create a reference to the 'posts' node in the database
-const postRef = ref(db, "posts")
-
-
+const postRef = ref(db, 'posts')
 
 const posts = [
   {
@@ -46,81 +52,78 @@ const posts = [
 ]
 //Store each post in the Firebase
 posts.forEach((post, index) => {
-  const newPostRef = ref(db, `posts/postId${index+1}`);
+  const newPostRef = ref(db, `posts/postId${index + 1}`)
   set(newPostRef, post)
 })
 posts.forEach((post, index) => {
-  const postRef = ref(db, `posts/postId${index+1}`)
+  const postRef = ref(db, `posts/postId${index + 1}`)
 
-  //ouvir firebase 
+  //ouvir firebase
   onValue(postRef, (snapshot) => {
-  const postData = snapshot.val(); 
-  
-  if (postData) {
-    posts[index].likes = postData.likes
-    console.log(index)
-    
-    /* document.querySelector(`.likes-${index+1}`).innerText = `${posts[index].likes} likes`; */
+    const postData = snapshot.val()
 
-  }
-});
-});
+    if (postData) {
+      posts[index].likes = postData.likes
+      console.log(index)
+
+      /* document.querySelector(`.likes-${index+1}`).innerText = `${posts[index].likes} likes`; */
+    }
+  })
+})
 
 // Handle dark mode toggle (optional)
-const toggle = document.getElementById('darkmode-toggle');
+const toggle = document.getElementById('darkmode-toggle')
 toggle.addEventListener('change', function () {
-  document.body.classList.toggle('dark-mode', toggle.checked);
-});
+  document.body.classList.toggle('dark-mode', toggle.checked)
+})
 
-const firstPostAvatar = document.querySelector("#post-avatar")
+const firstPostAvatar = document.querySelector('#post-avatar')
 console.log(firstPostAvatar)
 firstPostAvatar.src = 'images/avatar-vangogh.jpg'
 
-const namePostAvatar = document.querySelector("#avatar-name-1")
+const namePostAvatar = document.querySelector('#avatar-name-1')
 namePostAvatar.innerHTML = `<p class="bold avatar-name" id="avatar-name-1">${posts[0].name}</p> <br>
 <p class=avatar-location> ${posts[0].location}</p>
 `
 
 //interaction of the buttons wait and at the end build a real time database for this .
 
-const likes = document.querySelector('.likes-1');
+const likes = document.querySelector('.likes-1')
 
-const likeBtn = document.getElementById("like-btn")
-const commentBtn = document.querySelector('.comment-btn');
-const shareBtn = document.querySelector('.share-btn');
+const likeBtn = document.getElementById('like-btn')
+const commentBtn = document.querySelector('.comment-btn')
+const shareBtn = document.querySelector('.share-btn')
 
 //firebase
-const updateLikes = (postId, newLikes ) => {
+const updateLikes = (postId, newLikes) => {
   const postRef = ref(db, `posts/${postId}/likes`)
 
   set(postRef, newLikes)
     .then(() => {
-      console.log("likes updated in Firebase sucessfully");
+      console.log('likes updated in Firebase sucessfully')
     })
     .catch((error) => {
-      console.error("Error updating likes in Firebase", error);
-});
+      console.error('Error updating likes in Firebase', error)
+    })
 }
 
-
-likeBtn.addEventListener("click", () => {
-  const postId = 0;
-  posts[postId].likes++;
-    likes.innerText = posts[postId].likes + ` likes`
-    // Firebase
-    updateLikes(postId, posts[postId].likes)
-
-});
+likeBtn.addEventListener('click', () => {
+  const postId = 0
+  posts[postId].likes++
+  likes.innerText = posts[postId].likes + ` likes`
+  // Firebase
+  updateLikes(postId, posts[postId].likes)
+})
 
 commentBtn.addEventListener('click', () => {
-    alert('you want to comment')
-});
+  alert('you want to comment')
+})
 
 shareBtn.addEventListener('click', () => {
-    alert('You shared this post')
-});
+  alert('You shared this post')
+})
 
 //comments username and comment
 
 let comments = document.querySelector('.comments')
-comments.innerHTML = `<span class=bold >${posts[0].username} </span> <span> ${posts[0].comment}</span> `
+comments.innerHTML = `<span class="bold margin-bottom" >${posts[0].username} </span> <span> ${posts[0].comment}</span> `
