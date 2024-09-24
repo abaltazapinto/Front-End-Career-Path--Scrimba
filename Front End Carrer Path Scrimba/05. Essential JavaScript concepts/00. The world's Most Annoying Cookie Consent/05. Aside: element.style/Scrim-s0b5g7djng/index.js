5c33dba77723c50ff6a98e2437722348c0378835
abaltaza <andrebaltazarpinto@gmail.com>
2024-09-24 10:16:21 +0100

    challenge corrected

--- Front End Carrer Path Scrimba/05. Essential JavaScript concepts/00. The world's Most Annoying Cookie Consent/05. Aside: element.style/Scrim-s0b5g7djng/index.js
+++ Front End Carrer Path Scrimba/05. Essential JavaScript concepts/00. The world's Most Annoying Cookie Consent/05. Aside: element.style/Scrim-s0b5g7djng/index.js
@@ -1,9 +1,12 @@
 const revealBtn = document.getElementById('reveal-btn')
 const answer = document.getElementById('answer')
+const question = document.getElementById('question')
 
 revealBtn.addEventListener('click', function(){
     answer.style.display = 'block'
     revealBtn.style.display = 'none'
+    question.style.color = '#1434A4'
+    question.style.backgroundColor = '#68e1fd'
 })
 
 /*
