1120b5025a4b9055325d9585513402f7fce0518b
abaltaza <andrebaltazarpinto@gmail.com>
2024-09-25 11:44:35 +0100

    funny learning with the buttons row-reverse stuff needing display flex

--- Front End Carrer Path Scrimba/05. Essential JavaScript concepts/00. The world's Most Annoying Cookie Consent/26. Aside: ClassList Toggle Challenge/Scrim-s0hpm50ien/index.js
+++ Front End Carrer Path Scrimba/05. Essential JavaScript concepts/00. The world's Most Annoying Cookie Consent/26. Aside: ClassList Toggle Challenge/Scrim-s0hpm50ien/index.js
@@ -1,5 +1,6 @@
 const sortBtn = document.getElementById('sort-btn')
-const container =document.getElementById('container');            
+const container = document.getElementById('container');   
+
 sortBtn.addEventListener('click', function(){
 
 /*
