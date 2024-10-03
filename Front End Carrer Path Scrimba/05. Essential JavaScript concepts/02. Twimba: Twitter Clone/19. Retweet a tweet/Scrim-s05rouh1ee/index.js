71a91a4dbe99f8dcdf411308fad6d46b55dd6816
abaltaza <andrebaltazarpinto@gmail.com>
2024-10-03 10:11:44 +0100

    Solution of scrimba

--- Front End Carrer Path Scrimba/05. Essential JavaScript concepts/02. Twimba: Twitter Clone/19. Retweet a tweet/Scrim-s05rouh1ee/index.js
+++ Front End Carrer Path Scrimba/05. Essential JavaScript concepts/02. Twimba: Twitter Clone/19. Retweet a tweet/Scrim-s05rouh1ee/index.js
@@ -10,34 +10,54 @@ document.addEventListener('click', function(e){
     if(e.target.dataset.like){
        handleLikeClick(e.target.dataset.like) 
     }
+    else if(e.target.dataset.retweet){
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+/*
+Challenge:
+1. Make this eventListener call "handleRetweetClick" 
+   when the retweet icon is clicked, passing in the
+   uuid from that tweet.  
+*/
 })
 
 function handleLikeClick(tweetId){ 
     const targetTweetObj = tweetsData.filter(function(tweet){
         return tweet.uuid === tweetId
-    })[0];
+    })[0]
 
-    if(!targetTweetObj) return;
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }
+    else{
+        targetTweetObj.likes++ 
+    }
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render()
+}
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
     
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render()
+
 /*
 Challenge:
-1. When a tweet is liked, it's 'isLiked' property
-   should be set to true.
-2. When a tweet is unliked, it's 'isLiked' property
-   should be set to false and its 'likes' count
-   should be decremented.
-*/  
-    if(targetTweetObj.isLiked === false){
-        targetTweetObj.isLiked = true;
-        console.log("is what?:", targetTweetObj.isLiked)
-        targetTweetObj.likes++
-    }else {
-        targetTweetObj.isLiked = false;
-        targetTweetObj.likes--
-        console.log("is what ", targetTweetObj.isLiked)
-    }
-    
-    render()
+2. Find the retweeted tweet's object in tweetsData 
+   and save it to a const.
+3. Increment or decrement the retweet count of the 
+   tweet and flip its isRetweeted boolean.
+4. Call the render function.  
+*/   
 }
 
 function getFeedHtml(){
