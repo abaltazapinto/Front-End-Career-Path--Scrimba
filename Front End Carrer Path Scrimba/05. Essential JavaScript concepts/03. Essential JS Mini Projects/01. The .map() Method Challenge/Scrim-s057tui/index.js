b29ce0d21350e1246c1fc3ecf0fadd2c5d37eb7b
abaltaza <andrebaltazarpinto@gmail.com>
2024-10-04 13:45:14 +0100

    I just perfected the way i look to the map function

---
+++ Front End Carrer Path Scrimba/05. Essential JavaScript concepts/03. Essential JS Mini Projects/01. The .map() Method Challenge/Scrim-s057tui/index.js
@@ -0,0 +1,34 @@
+import { playlistArr } from './playlist.js'
+
+/*
+Challenge
+1. Refactor the code below to use .map() 
+   instead of the for loop.
+   ⚠️ Don't worry about the commas for now.
+*/
+
+const playlistHtml = []
+
+
+playlistArr.map(function(playlistArr) {
+    playlistHtml.unshift(
+            `<section class="card">
+                <div class="card-start">
+                    <img src="./images/${playlistArr.albumArt}">
+                </div>
+                    <div class="card-mid">
+                        <h4 class="card-title">${playlistArr.title}</h4>
+                        <p class="card-artist">${playlistArr.artist}</p>
+                    </div>
+                <div class="card-end">
+                    <p class="card-menu">...</p>
+                </div>
+            </section>
+                `
+    )
+
+    
+})
+
+
+document.getElementById('container').innerHTML = playlistHtml.join('')
