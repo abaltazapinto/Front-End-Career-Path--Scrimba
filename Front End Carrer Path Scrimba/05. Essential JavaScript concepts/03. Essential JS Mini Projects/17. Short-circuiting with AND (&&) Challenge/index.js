447880a2575b1998d3807b5da3d14cb77f35ed4d
abaltaza <andrebaltazarpinto@gmail.com>
2024-10-06 21:18:01 +0100

    after the challenge correction

---
+++ Front End Carrer Path Scrimba/05. Essential JavaScript concepts/03. Essential JS Mini Projects/17. Short-circuiting with AND (&&) Challenge/index.js
@@ -0,0 +1,28 @@
+const accountBalanceUsd = '$45,000,000,000 🤑💰'
+const swissBankPassCodesArr = [1234,5678,9876,3434]
+
+function authenticationCheck(passCode){
+/*
+Challenge:
+1. If the passcode passed into authenticationCheck 
+   exists in swissBankPassCodesArr, authenticationCheck 
+   should log out accountBalanceUsd. 
+   
+   If the passcode does not exist in swissBankPassCodesArr 
+   then authenticationCheck need not do anything.
+   
+⚠️ Make sure you short-circuit with &&
+   hint.md for help!
+*/
+
+const passcodes = [...swissBankPassCodesArr]
+
+const passCheck = passcodes.includes(passCode)
+console.log(passCheck && passcodes)
+
+if(passCheck && passcodes) {
+    console.log(accountBalanceUsd)
+}
+}
+
+authenticationCheck(3435)
