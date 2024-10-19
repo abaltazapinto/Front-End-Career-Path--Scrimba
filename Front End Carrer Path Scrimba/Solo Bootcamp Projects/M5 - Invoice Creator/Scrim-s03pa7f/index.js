ef063cde761da23e908f7efc040dd01c270d5f12
abaltaza <andrebaltazarpinto@gmail.com>
2024-10-19 17:16:43 +0100

    just trying to add one and ask if they want the second

--- Front End Carrer Path Scrimba/Solo Bootcamp Projects/M5 - Invoice Creator/Scrim-s03pa7f/index.js
+++ Front End Carrer Path Scrimba/Solo Bootcamp Projects/M5 - Invoice Creator/Scrim-s03pa7f/index.js
@@ -1,3 +1,4 @@
+
 import { workItems } from './assets/workItems.js'
 
 // Handle dark mode toggle
@@ -27,7 +28,6 @@ let currentIndex = 0;
 const itemsPerPage = 3;
 const itemsToSlide = 1;
 let selectedItems = [];
-
 
 // carousel
 function initCarousel() {
@@ -64,6 +64,7 @@ function addToInvoice(e) {
     let priceNumber = itemPrice ? parseFloat(itemPrice) : 0;
 
     // Update itemCounts for the clicked item
+    
     if (!itemCounts[itemName]) {
         itemCounts[itemName] = 0; // Initialize count if not present
     }
@@ -73,9 +74,13 @@ function addToInvoice(e) {
     let existingItem = selectedItems.find(i => i.name === itemName);
 
     if (existingItem) {
+
         // Update count and total price if the item already exists
-        existingItem.count = itemCounts[itemName];
-        existingItem.totalPrice = existingItem.count * existingItem.price;
+        if (existingItem.count === 0 && existingItem.count === 1){
+            existingItem.count = itemCounts[itemName];
+            existingItem.totalPrice = existingItem.count * existingItem.price;
+        }
+
     } else {
         // If item doesn't exist, add it to selectedItems with initial count and total price
         selectedItems.push({
@@ -90,102 +95,159 @@ function addToInvoice(e) {
     const taskContainer = document.getElementById('selected-items');
     const countContainer = document.getElementById('selected-items-count')
     // Clear the current contents of the container to avoid duplications
-    taskContainer.innerHTML = '';
-    countContainer.innerHTML = '';
+    
+    if(existingItem) {
 
+        const grabContainer = document.getElementById('container')
+        
+        // alert(`You have already a ${itemName} you want another one ? ` )
+        const sectionWarningDetails = document.createElement("section")
+        sectionWarningDetails.classList.add('warning-details') 
+
+        const mainWarningArea = document.createElement('main')
+        mainWarningArea.classList.add('main-Warning-area')
+
+        const areaButtons = document.createElement('div');
+        areaButtons.classList.add('area-buttons')
+
+        const closeButton = document.createElement('span');
+        closeButton.classList.add('close')
+        
+
+        //close the modal on clicking the close button
+        closeButton.onclick = function () {
+            modalWarningQuestion.style.display = 'none';
+        }
+
+
+        const modalWarningQuestion = document.createElement('h2')
+        modalWarningQuestion.textContent = DOMPurify.sanitize(`Do you want to add anoter ${itemName} to the services ?`)
+        modalWarningQuestion.setAttribute('aria-label', "Question about the number of services need");
+        
+        grabContainer.appendChild(sectionWarningDetails)
+        mainWarningArea.appendChild(modalWarningQuestion)
+        mainWarningArea.appendChild(areaButtons)
+   
+        sectionWarningDetails.appendChild(mainWarningArea)
+
+        const yesButton = document.createElement('button');
+        yesButton.textContent = 'Yes';
+        yesButton.onclick = function () {
+            existingItem.count = itemCounts[itemName];
+            existingItem.totalPrice = existingItem.count * existingItem.price;
+            (function() {     
+                console.log("came here")
+                // taskContainer.innerHTML = '';
+                // countContainer.innerHTML = '';
+                // Iterate over `selectedItems` and add them to the container in the HTML
+                for (let item of selectedItems) {
+                        
+                        if(item.count < 2) {
+                            // Create a new HTML element for each item (e.g., a 'div')
+                            const itemElement = document.createElement('div');
+                            itemElement.classList.add('item-element');
+                            // Create a separate element for the ite-name
+                            const itemNameElement = document.createElement('div')
+                            itemNameElement.classList.add('item-name');
+                            itemNameElement.textContent = `${item.name.charAt(0).toUpperCase() + item.name.slice(1)}`;
+                            taskContainer.appendChild(itemElement);
+                            itemElement.appendChild(itemNameElement);
+                            
+                            // Need to create a new span for the count and total price
+                            const itemCount = document.createElement('div')
+                            itemCount.classList.add('item-count')
+
+                            console.log("passed in the second component")
+                            const totalCountElement = document.createElement('div');
+
+                            totalCountElement.classList.add('item-count');
+                            totalCountElement.classList.add(`data-${itemName}`);
+                            // totalCountElement.id = `${item.name.toLowerCase().replace(/\s/g, '-')}-total`;
+                            totalCountElement.textContent = `x ${item.count} - $${item.totalPrice.toFixed(2)}`
+                            
+                            countContainer.appendChild(itemCount);
+                            itemCount.appendChild(totalCountElement)
+                            taskDiv.appendChild(taskContainer)
+
+                            console.log("item.count", item.count)
+
+                            const totalItemsAmount = selectedItems.reduce((sum, item) => sum + item.totalPrice, 0)
+                            document.getElementById('totalAmount').textContent =`$${totalItemsAmount.toFixed(2)}`;
+                        } else {
+
+                            existingTotalCountElement = document.querySelector(`data-${itemName}`);
+                            console.log(existingTotalCountElement)
+                            if(existingTotalCountElement) {
+                                totalCountElement.innerHTML = '';
+                                totalCountElement.textContent = `x ${item.count} - $${item.totalPrice.toFixed(2)}`
+                            }    
+                        }
+            }
+            
+            sectionWarningDetails.style.display = 'none';
+        })();
+            // updateInvoice();
+
+        }
+
+        const noButton = document.createElement('button');
+        noButton.textContent = 'No';
+        noButton.onclick = function () {
+            sectionWarningDetails.style.display = 'none';
+        }
+
+        areaButtons.appendChild(yesButton);
+        areaButtons.appendChild(noButton);
+        
+            // Close the modal when clicking outside of it
+            window.onclick = function (event) {
+                if (event.target === window) {
+                    mainWarningArea.style.display = 'none';
+                }
+            };
+                
+    }
+
+    if (!existingItem) {
+        taskContainer.innerHTML = '';
+        countContainer.innerHTML = '';
     // Iterate over `selectedItems` and add them to the container in the HTML
     for (let item of selectedItems) {
+        if(item.count >0 && item.count <2){
         // Create a new HTML element for each item (e.g., a 'div')
         const itemElement = document.createElement('div');
         itemElement.classList.add('item-element');
-
         // Create a separate element for the ite-name
         const itemNameElement = document.createElement('div')
         itemNameElement.classList.add('item-name');
-        itemNameElement.textContent = `${item.name.charAt(0).toUpperCase() + item.name.slice(1)}`;
 
+        itemNameElement.textContent = `${item.name.charAt(0).toUpperCase() + item.name.slice(1)}`;
         taskContainer.appendChild(itemElement);
         itemElement.appendChild(itemNameElement);
 
+            console.log("passed in the second conponent")
+
         // Need to create a new span for the count and total price
         const itemCount = document.createElement('div')
-        itemCount.classList.add('item-count')
+        itemCount.classList.add('item-count');
+        itemCount.classList.add(`data-${itemName}`);
 
         const totalCountElement = document.createElement('div');
         totalCountElement.classList.add('item-count');
-        totalCountElement.textContent = `x ${item.count} - $${item.totalPrice.toFixed(2)}`
-
-        countContainer.appendChild(itemCount);
-        itemCount.appendChild(totalCountElement)
-        taskDiv.appendChild(taskContainer)
-        
+            totalCountElement.textContent = `x ${item.count} - $${item.totalPrice.toFixed(2)}`
+            countContainer.appendChild(itemCount);
+            itemCount.appendChild(totalCountElement)
+            taskDiv.appendChild(taskContainer)
+            console.log("item.count", item.count)
+            const totalItemsAmount = selectedItems.reduce((sum, item) => sum + item.totalPrice, 0)
+            document.getElementById('totalAmount').textContent =`$${totalItemsAmount.toFixed(2)}`;
+        }
+        // for the total
+        console.log(itemCounts && item.count)
     }
+}
     
-    // for the total
-    const totalItemsAmount = selectedItems.reduce((sum, item) => sum + item.totalPrice, 0)
-    document.getElementById('totalAmount').textContent =`$${totalItemsAmount.toFixed(2)}`;
 }
-// add more-work + button
-function initializeMenuWork() {
-    const addWork = document.querySelector('.add-work')
-
-    // if (addWork) {
-    //     addWork.addEventListener("click", () => {
-    //     //  const sectionCarousel = document.querySelector('.carousel-container')
-    //     //  sectionCarousel.style.display = 'none'
-    //     // //  sectionCarousel.classList.add('hidden') // hide the carousel
-
-    //     //  // create new section to add work
-    //     //  const sectionPrevTowork = document.querySelector(".container");
-    //     //  const sectionAddWorkContainer = document.createElement('section')
-    //     //  sectionAddWorkContainer.classList.add('article-work-container');
-
-    //     //  console.log(sectionPrevTowork)
-    //     //  console.log(sectionAddWorkContainer.parentElement)
-
-    //     //  // create the elements of input and button 
-    //     //  const taskInput = document.createElement('input');
-    //     //  taskInput.type = 'text';
-    //     //  taskInput.setAttribute = ('placeholder', 'Enter a task');
-    //     //  taskInput.classList.add('task-input'); // see the css to style this class
-         
-    //     //  const taskPrice = document.createElement('input');
-    //     //  taskPrice.type = 'number';
-    //     //  taskPrice.setAttribute = ('placeholder', '$10');
-    //     //  taskPrice.value = 10;
-    //     //  taskPrice.classList.add('task-price'); // style this class
-
-    //     //  const taskAddButton = document.createElement('button');
-    //     //  taskAddButton.classList.add('task-add-btn');
-    //     //  taskAddButton.textContent = '+'
-
-    //     //  //add the elements to the main container
-    //     //     sectionAddWorkContainer.appendChild(taskInput)
-    //     //     sectionAddWorkContainer.appendChild(taskPrice)
-    //     //     sectionAddWorkContainer.appendChild(taskAddButton)
-
-    //     //  document.body.appendChild(sectionAddWorkContainer)
-
-    //     //  // Hipotese de adionar o functionamento de adicionar tarefa
-    //     //  taskAddButton.addEventListener('click', () => {
-    //     //     console.log('Task added', taskInput.value, taskPrice.value);
-    //     //     // aqui posso implementar a lógica para adiconar tarefa ao invoice
-    //     //  })
-    //     // });
-    // }else {
-    //     console.error("Elemento '.add-work' não encontrado. ");
-    // }
-
-}
-
-initializeMenuWork();
-
-function addWorkMenu() {
-    console.log('You made it')
-    initializeMenuWork()
-}
-
-
 
 // create the invoice
 sendInvoiceBtn.addEventListener('click', () => {
@@ -196,7 +258,6 @@ sendInvoiceBtn.addEventListener('click', () => {
     // updateInvoice();
     updateInvoice();
 });
-
 
 prevButton.addEventListener('click', () => {
     if (currentIndex > 0) {
