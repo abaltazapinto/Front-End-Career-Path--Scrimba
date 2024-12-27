dd1915129bb862f4c690234d0d3f4596951fa406
abaltaza <andrebaltazarpinto@gmail.com>
2024-12-27 14:29:15 +0000

    Review done

--- Front End Carrer Path Scrimba/Solo Projects/M6 - Learning journal/Scrim-s03mi9m/js/index.js
+++ Front End Carrer Path Scrimba/Solo Projects/M6 - Learning journal/Scrim-s03mi9m/js/index.js
@@ -6,6 +6,7 @@ console.log("afdsuihdsa");
 //   "aria-label",
 //   "Toggle between the two theme possibles dark and light",
 // );
+
 toggle.addEventListener("change", function () {
     document.body.classList.toggle("dark-mode", toggle.checked);
   });
