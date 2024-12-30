3c0fc5884d32de5f4f4e44e234c9dc95c88a70f2
abaltaza <andrebaltazarpinto@gmail.com>
2024-12-30 12:11:04 +0000

    Crazy things response => response.json()

--- Front End Carrer Path Scrimba/08. Working with APIs/01. Intro to APIs & BoredBot/12. Fetch idea from Bored Api/Scrim-s01ubdd/index.js
+++ Front End Carrer Path Scrimba/08. Working with APIs/01. Intro to APIs & BoredBot/12. Fetch idea from Bored Api/Scrim-s01ubdd/index.js
@@ -1,24 +1,24 @@
-// /**
-// Challenge: 
+/**
+Challenge: 
 
-// 1. Fetch a random activity from the Bored API
-// url: https://apis.scrimba.com/bored/api/activity
+1. Fetch a random activity from the Bored API
+url: https://apis.scrimba.com/bored/api/activity
 
-// 2. Display the text of the activity in the browser
-// */
+2. Display the text of the activity in the browser
+*/
 
 
-// fetch("https://apis.scrimba.com/bored/api/activity")
-//     .then(response => response.json)
-//     .then(data => {
-//         const textFetched = data.activity;
-//         const text = document.createElement('p');
+fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        const textFetched = data.activity;
+        const text = document.createElement('p');
 
-//         text.textContent = textFetched.data;
+        text.textContent = textFetched;
 
-//         const container = document.getElementById("text-container");
-//         container.appendChild(text)
-//         console.log(data);
-//     })
+        const container = document.getElementById("text-container");
+        container.appendChild(text)
+        console.log(data);
+    })
 
-// my answer 
+// crazy life just forget it to out a a brackets at the end of it () response => response.json()
