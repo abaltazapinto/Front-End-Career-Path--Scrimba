5873409880fb6463512dce1e7677811e12aef7bd
abaltaza <andrebaltazarpinto@gmail.com>
2024-12-30 11:56:16 +0000

    my answer

---
+++ Front End Carrer Path Scrimba/08. Working with APIs/01. Intro to APIs & BoredBot/12. Fetch idea from Bored Api/Scrim-s01ubdd/index.js
@@ -0,0 +1,24 @@
+// /**
+// Challenge: 
+
+// 1. Fetch a random activity from the Bored API
+// url: https://apis.scrimba.com/bored/api/activity
+
+// 2. Display the text of the activity in the browser
+// */
+
+
+// fetch("https://apis.scrimba.com/bored/api/activity")
+//     .then(response => response.json)
+//     .then(data => {
+//         const textFetched = data.activity;
+//         const text = document.createElement('p');
+
+//         text.textContent = textFetched.data;
+
+//         const container = document.getElementById("text-container");
+//         container.appendChild(text)
+//         console.log(data);
+//     })
+
+// my answer 
