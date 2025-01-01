084784fd6faa69363b82968304272d982667d533
abaltaza <andrebaltazarpinto@gmail.com>
2025-01-01 10:17:41 +0000

    just adding a new request through fetch

---
+++ Front End Carrer Path Scrimba/08. Working with APIs/02. URLs, REST, & BlogSpace/HTTP requests - ex 1/Scrim-s01pthf/index.js
@@ -0,0 +1,18 @@
+/**
+ * Challenge:
+ * 
+ * Send a request to the JSON Placeholder API using `fetch`
+ * URL: https://apis.scrimba.com/jsonplaceholder/posts
+ * 
+ * Documentation: https://jsonplaceholder.typicode.com/
+ * 
+ * Log the response data to the console
+ */
+
+document.addEventListener("click", () => {
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+})
