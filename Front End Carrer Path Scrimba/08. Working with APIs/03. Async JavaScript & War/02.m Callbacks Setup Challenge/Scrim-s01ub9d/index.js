30c6bfbd133d5ff41628160a5be7c0471957abd5
abaltaza <andrebaltazarpinto@gmail.com>
2025-01-14 19:30:38 +0000

    After reviewing the response it needed a button

--- Front End Carrer Path Scrimba/08. Working with APIs/03. Async JavaScript & War/02.m Callbacks Setup Challenge/Scrim-s01ub9d/index.js
+++ Front End Carrer Path Scrimba/08. Working with APIs/03. Async JavaScript & War/02.m Callbacks Setup Challenge/Scrim-s01ub9d/index.js
@@ -6,6 +6,8 @@
  Log the whole response to the console
  */
 
-fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
-    .then(res => res.json())
-    .then(data => console.log(data))
+document.getElementById("new-deck").addEventListener("click", function() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => console.log(data)) 
+})
