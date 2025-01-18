71c7a7de351214bf2d32603d117f2c8b6fb01d6f
abaltaza <andrebaltazarpinto@gmail.com>
2025-01-18 19:34:13 +0000

    determine the winner

---
+++ Front End Carrer Path Scrimba/08. Working with APIs/03. Async JavaScript & War/15- War - Determine the winning card/Scrim-s01uba2/index.js
@@ -0,0 +1,83 @@
+let deckId
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            deckId = data.deck_id
+        })
+}
+
+newDeckBtn.addEventListener("click", handleClick)
+
+function cardWinner(card1, card2)
+{
+    const cardValues = {
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "JACK": 11,
+        "QUEEN": 12,
+        "KING": 13,
+        "ACE": 14,
+    }
+    
+    const card1Value = cardValues[card1.value];
+    const card2Value = cardValues[card2.calue];
+    
+    (card1Value < card2Value) ? "card1" : (card1Value < card2Value) ? "card2" : "tie";
+    
+}
+
+drawCardBtn.addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            cardsContainer.children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `
+            cardsContainer.children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `
+            
+            const winner = cardWinner(card1, card2);
+            console.log("Winner:", winner);
+        })
+})
+
+/**
+ * Challenge:
+ * 
+ * Try to determine which of the 2 cards is the "winner" (has higher value)
+ * Aces are the card with the highest "score"
+ * 
+ * In parts:
+ * 
+ * 1. Create a function that takes 2 card objects as parameters, 
+ * `card1` and `card2`. These card objects have a property called
+ * `value`, which can be any one of the following strings, in
+ * order of rising "score":
+ * 
+ *
+ * 
+ * "2", "3", "4", "5", "6", "7", "8", "9", 
+ * "10", "JACK", "QUEEN", "KING", "ACE"
+ * 
+ * I.e. "2" is the lowest score and "ACE" is the highest.
+ * 
+ * The function should determine which of the 2 cards (`card1`
+ * or `card2`) has the higher score, or if they have the same score.
+ * 
+ * Log which card wins (or "It's a tie!" 
+ * if they're the same) to the console
+ */
