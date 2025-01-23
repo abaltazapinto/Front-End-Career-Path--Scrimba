1a7a07bea594978a0806ca5244b77824daee5e88
abaltaza <andrebaltazarpinto@gmail.com>
2025-01-23 19:36:47 +0000

    going with the watch list project

---
+++ Front End Carrer Path Scrimba/Solo Projects/M8 - MovieWatchlist/index.js
@@ -0,0 +1,29 @@
+// Handle dark mode toggle
+const toggle = document.getElementById("darkmode-toggle");
+console.log(toggle);
+const label = document.getElementById("toggle-label");
+console.log(label);
+
+toggle.setAttribute(
+  "aria-label",
+  "Toggle between the two theme possibles dark and light",
+);
+
+toggle.addEventListener("change", function () {
+    document.body.classList.toggle("dark-mode", toggle.checked);
+});
+
+/*
+
+my key = http://www.omdbapi.com/?i=tt3896198&apikey=8d3707a7
+
+*/
+
+async function handleSearch() {
+    const res = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=8d3707a7")
+    const data = await res.json()
+
+    console.log(data)
+}
+
+handleSearch();
