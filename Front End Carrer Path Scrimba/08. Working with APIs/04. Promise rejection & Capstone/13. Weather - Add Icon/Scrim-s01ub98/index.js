5dcf3d79d6f35c3a7f212b54c1fc8a897f3c6079
abaltazapinto <andrebaltazarpinto@gmail.com>
2025-03-19 15:02:28 +0000

    exercise done

--- Front End Carrer Path Scrimba/08. Working with APIs/04. Promise rejection & Capstone/13. Weather - Add Icon/Scrim-s01ub98/index.js
+++ Front End Carrer Path Scrimba/08. Working with APIs/04. Promise rejection & Capstone/13. Weather - Add Icon/Scrim-s01ub98/index.js
@@ -63,3 +63,41 @@ navigator.geolocation.getCurrentPosition(position => {
  * an array of weather for that area. You can just access the first
  * item in that array to get the icon ID.
  */
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        .then(res => {
+            if(!res.ok){
+                throw Error("Weather data not available")
+            }
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            const iconCode = data.weather[0].icon;
+            console.log("Icon Code", iconCode);
+            const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png` // Construct the icon URL
+            console.log("iconUrl", iconUrl);
+            const weatherDiv = document.getElementById("weather");
+            const img = document.createElement("img");
+            img.src = iconUrl;
+            img.alt = "Weather Icon";
+            const iconTemp = document.createElement('div');
+            iconTemp.id = "iconTemp";
+            iconTemp.appendChild(img);
+            weatherDiv.appendChild(iconTemp);
+            const paragraph = document.createElement('p');
+            const temperature = Math.round(data.main.temp); // Obtenha temperatura do objeto data.
+            paragraph.textContent = `${temperature}ºF`;
+            iconTemp.appendChild(paragraph);
+            weatherDiv.appendChild(iconTemp);
+            const location = document.createElement('div');
+            location.id = "nameLocation"
+            location.alt = "Location of the place";
+            const nameOfTheLocation = (data.name);
+            location.textContent =`${nameOfTheLocation}`;
+            weatherDiv.appendChild(location);
+            
+        })
+        .catch(err => console.error(err))
+    });
