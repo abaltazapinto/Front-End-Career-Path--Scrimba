634c17d2f8d3bfe29b653a85b73e88c457f711d2
abaltazapinto <andrebaltazarpinto@gmail.com>
2025-09-30 14:48:19 +0100

    React Logo

--- Front End Carrer Path Scrimba/10. React Basics/06. Why React? Its declarative/Scrim-s06ieda/index.jsx
+++ Front End Carrer Path Scrimba/10. React Basics/06. Why React? Its declarative/Scrim-s06ieda/index.jsx
@@ -25,3 +25,24 @@ h1.className = "header"
 
 document.getElementById("root").appendChild(h1)
 
+/*
+
+Another test
+/*
+
+/**
+ * Challenge: set up a new React app from scratch!
+ * Hints:
+ * 1. Need to import the method to create a new root
+ *    from the "react-dom/client" package)
+ * 2. Use that root to render some JSX. For now, just render
+ *    an h1 element that says anything you want.
+ */
+
+//import { createRoot } from "react-dom/client"
+
+//const root = createRoot(document.getElementById("root"))
+
+//root.render(
+//    <h1> I am learning React </h1>
+//)
