33e7f602bd0d0f1aec8ef3ab86a437349b665289
abaltazapinto <andrebaltazarpinto@gmail.com>
2025-10-17 18:13:18 +0100

    baby steps

--- Front End Carrer Path Scrimba/Solo Projects/M9 - Digital Business Card React/Digital-Business-Card/src/App.jsx
+++ Front End Carrer Path Scrimba/Solo Projects/M9 - Digital Business Card React/Digital-Business-Card/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,13 +6,39 @@ import AndreImage from './assets/Andre_Pinto.jpg'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+	document.body.classList.toggle('dark-mode', darkMode)
+  }, [darkMode])
 
   return (
     <>
+	  {/* Main content */ }
 	<main >
 		<div className='Card'>
-      	<img src={AndreImage} alt='Andre Pinto' />
+      		<img src={AndreImage} alt='Andre Pinto' />
+			<div className='Card--info'>
+				<h2 className='Card--name'>Andre Pinto</h2>
+				<h3 className='Card--job-title'>Frontend Developer</h3>
+				<p className='Card--website'>andre-pinto.website</p>
+				<div className='Card--buttons'>
+					<button className='Card--email-button'>
+						<i className="fa-solid fa-envelope"></i> Email
+					</button>
+					<button className='Card--linkedin-button'>
+						<i className="fa-brands fa-linkedin"></i> LinkedIn
+					</button>
+				</div>
+				<div className='Card--about'>
+					<h4>About</h4>
+					<p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+				</div>
+				<div className='Card--interests'>
+					<h4>Interests</h4>
+					<p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+				</div>
+			</div>
 		</div>
 	</main>
     </>
