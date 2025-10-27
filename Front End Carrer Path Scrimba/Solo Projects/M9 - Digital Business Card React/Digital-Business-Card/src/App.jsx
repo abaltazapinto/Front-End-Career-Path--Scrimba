2feff00063437d0163349df34bd0e899ba8c3a13
abaltazapinto <andrebaltazarpinto@gmail.com>
2025-10-27 19:25:36 +0000

    improvements

--- Front End Carrer Path Scrimba/Solo Projects/M9 - Digital Business Card React/Digital-Business-Card/src/App.jsx
+++ Front End Carrer Path Scrimba/Solo Projects/M9 - Digital Business Card React/Digital-Business-Card/src/App.jsx
@@ -5,7 +5,8 @@ import './App.css'
 import AndreImage from './assets/Andre_Pinto.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faSquareGithub, faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
 
 
 function App() {
@@ -23,14 +24,19 @@ function App() {
       		<img src={AndreImage} alt='Andre Pinto' />
 			<div className='Card--info'>
 				<h2 className='Card--name'>Andre Pinto</h2>
-				<h3 className='Card--job-title'>Frontend Developer</h3>
-				<p className='Card--website'>andre-pinto.website</p>
+				<h3 className='Card--job-title'>Developer</h3>
 				<div className='Card--buttons'>
 					<button className='Card--email-button'>
 						<FontAwesomeIcon icon={faEnvelope} /> Email
 					</button>
 					<button className='Card--linkedin-button'>
-						<i className="fa-brands fa-linkedin"></i> LinkedIn
+						<a
+							href="https://www.linkedin.com/in/andr%C3%A9-baltazar-pinto-13a056141/"
+							target="_blank"
+							rel="noopener noreferrer"
+							className='Card--linkedin-link'>
+						<FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon> LinkedIn
+						</a>
 					</button>
 				</div>
 				<div className='Card--about'>
@@ -41,7 +47,22 @@ function App() {
 					<h4>Interests</h4>
 					<p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
 				</div>
-			</div>
+
+				</div>
+					<div className='Card--footer'>
+						<a href="https://x.com/jacaralho1" target="_blank" rel="noopener noreferrer">
+						<FontAwesomeIcon icon={faSquareTwitter} />
+						</a>
+						<a href="https://www.linkedin.com/in/andr%C3%A9-baltazar-pinto-13a056141/" target="_blank" rel="noopener noreferrer">
+						<FontAwesomeIcon icon={faLinkedin} />
+						</a>
+						<a href="https://www.instagram.com/andrebaltazarj/" target="_blank" rel="noopener noreferrer">
+						<FontAwesomeIcon icon={faSquareInstagram}/>
+						</a>
+						<a href='https://github.com/abaltazapinto' target="_blank" rel="noopener noreferrer" >
+						<FontAwesomeIcon icon={faSquareGithub} />
+						</a>
+				</div>
 		</div>
 	</main>
     </>
